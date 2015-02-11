2130a404111ee035e9c13160cd136fdeac3024f0
Madara Uchiha <me@madara.ninja>
2015-02-11 18:52:56 +0200

    add basic knockout binding

--- client/js/main.js
+++ client/js/main.js
@@ -1,7 +1,21 @@
 class AppViewModel {
     constructor() {
-        this.defaultAnimeName = 'Naruto';
+        this.animeName = ko.observable();
     }
+    loadSearchResults() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('get', '/search?' + urlEncodeObject({name: this.animeName()}));
+        xhr.onload = () => { this.renderSearchResults(xhr.responseText) };
+        xhr.send();
+    }
+
+    renderSearchResults(response) {
+
+    }
+}
+
+function urlEncodeObject(obj) {
+    return Object.keys(obj).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(obj[key])).join('&');
 }
 
 ko.applyBindings(new AppViewModel());
