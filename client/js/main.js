52b3e19aad08b1c2313305c6c1412a5b246ca14c
Dor Tchizik <dor.t@taboola.com>
2015-02-12 11:01:33 +0200

    Refactor client-side code to use promises

--- client/js/main.js
+++ client/js/main.js
@@ -1,21 +1,41 @@
 class AppViewModel {
     constructor() {
         this.animeName = ko.observable();
+        this.results = ko.observable();
     }
     loadSearchResults() {
-        let xhr = new XMLHttpRequest();
-        xhr.open('get', '/search?' + urlEncodeObject({name: this.animeName()}));
-        xhr.onload = () => { this.renderSearchResults(xhr.responseText) };
-        xhr.send();
+        return ajax("/search?", "GET", {name: this.animeName()}).then(res => {
+            this.renderSearchResults(res);
+        });
     }
 
     renderSearchResults(response) {
-
+        let data = JSON.parse(response);
+        this.items = data.map(el => new SearchResult(el.title, el.url));
+        console.log(this.items);
     }
 }
 
-function urlEncodeObject(obj) {
-    return Object.keys(obj).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(obj[key])).join('&');
+class SearchResult {
+    constructor(title, url) {
+        this.title = title;
+        this.url = url;
+    }
+}
+
+function ajax(url, method = "GET", data = null){
+    var xhr = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = reject;
+        if(method.toUpperCase() === "GET") url += "?" + urlEncodeObject(data);
+        xhr.open(method, url);
+        xhr.send(data);
+    });
+
+    function urlEncodeObject(obj) {
+        return Object.keys(obj).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(obj[key])).join('&');
+    }
 }
 
 ko.applyBindings(new AppViewModel());
